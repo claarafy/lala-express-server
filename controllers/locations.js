c23fa1f58d0bb1dbdc1ade220271b1161cc0751c
Clara Kang <clara.kang91@gmail.com>
2017-06-20 22:49:45 -0700

    location controller edited

--- controllers/locations.js
+++ controllers/locations.js
@@ -23,7 +23,6 @@ module.exports = {
     console.log("from server, logging the request", req.body.name, req.body.coordinates) //req.body.name= city name or address
     var location
     Location.findOne({name: req.body.name}, (err, location) => {
-      console.log("lower cased location", location.name);
       if(location) {
         console.log("this location already exists in the data")
         return res.json({success: true, message: "this location already exists in the data", location})
